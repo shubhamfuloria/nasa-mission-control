55d9a4e32a22397c8dad195f93d84a5d290e84d5
shubhamfuloria <sfuloria10200@gmail.com>
2023-01-28 18:45:27 +0530

    added pipeline workflow

--- server/src/routes/launches/launches.spec.js
+++ server/src/routes/launches/launches.spec.js
@@ -2,10 +2,12 @@ const request = require("supertest");
 const app = require("../../app");
 const { response } = require("../../app");
 const { mongoConnect, mongoDisconnect } = require("../../services/mongo");
+const { loadPlanetsData } = require("../../models/planets.model");
 
 describe("test launches api", () => {
   beforeAll(async () => {
     await mongoConnect();
+    await loadPlanetsData(); 
   });
 
   afterAll(async () => {
